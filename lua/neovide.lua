fc953e4cb5f4ed4ef78ddd8d0f17d916d7e4c339
pieeep <gayp0rn@cash4u.com>
2023-01-31 01:35:15 +0100

    more stuff

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -2,8 +2,14 @@
 -- neovide --
 -------------
 
-vim.opt.guifont = { 'SauceCodePro NF', 'h14' }
+vim.opt.guifont = { 'SauceCodePro NF', 'h12' }
 
 vim.g.neovide_transparency = 0.85
+
+-- cursor
 vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_cursor_animation_length = 0.0
+
+vim.g.neovide_refresh_rate = 60
+vim.g.neovide_refresh_rate_idle = 5
 
