16520a26c197d7b6f4c542bccd982928e207ee26
Ruben Lauwereys <gayp0rn@cash4u.com>
2023-01-31 02:51:18 +0100

    all the stuff

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -2,10 +2,10 @@
 -- neovide --
 -------------
 
-vim.opt.guifont = { 'SauceCodePro NF', 'h12' }
+vim.opt.guifont = { 'JetBrainsMono NF', 'h12' }
 vim.g.neovide_scale_factor = 0.8
 
-vim.g.neovide_transparency = 1.0
+vim.g.neovide_transparency = 0.85
 
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_cursor_animation_length = 0.0
