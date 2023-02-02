a99584a1b2324b7d19a57fcb44a2927d6b134b68
Ruben Lauwereys <gayp0rn@cash4u.com>
2023-02-02 05:07:30 +0100

    various changes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,25 +6,22 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- empty setup using defaults
-require('nvim-tree').setup()
-
--- OR setup with some options
--- require('nvim-tree').setup({
---   sort_by = 'case_sensitive',
---   view = {
---     adaptive_size = true,
---     mappings = {
---       list = {
---         { key = 'u', action = 'dir_up' },
---       },
---     },
---   },
---   renderer = {
---     group_empty = true,
---   },
---   filters = {
---     dotfiles = true,
---   },
--- })
+require('nvim-tree').setup({
+  view = {
+    width = 25,
+    preserve_window_proportions = true,
+  },
+  renderer = {
+    add_trailing = true,
+    highlight_git = true,
+    highlight_opened_files = "name",
+    root_folder_label = function(path)
+      return '../' .. vim.fn.fnamemodify(path, ':t') .. '/'
+    end,
+    indent_markers = {
+      enable = true,
+      icons = { item = '├'},
+    },
+  },
+})
 
