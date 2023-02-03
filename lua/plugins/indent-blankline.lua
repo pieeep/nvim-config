753c3b684ff4da81510924ad8c76fd9b71580768
Ruben Lauwereys <gayp0rn@cash4u.com>
2023-02-04 00:19:17 +0100

    changed setup

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -7,6 +7,7 @@ vim.opt.listchars:append 'space:⋅'
 
 require('indent_blankline').setup({
   show_current_context = true,
+  space_char_blankline = ' ',
   use_treesitter = true,
   filetype_exclude = {
     'haskell',
