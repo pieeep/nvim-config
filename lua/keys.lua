65ca9be4dd08e5f26d0165f0dcb7ec3aad789cfe
Ruben Lauwereys <gayp0rn@cash4u.com>
2023-02-03 20:46:14 +0100

    removed more redundancy

--- lua/keys.lua
+++ lua/keys.lua
@@ -14,23 +14,19 @@ vim.keymap.set('n', '<leader>so', ':so %<CR>')
 
 -- automatically complete quotes/brackets
 local function complete(char, single)
-  local result = single and char:rep(2)..'<left>' or char
+  local result = single and char:rep(2) .. '<left>' or char
   local col = vim.fn.col('.')
   local current_char = vim.fn.getline('.'):sub(col, col)
   return current_char == char and '<right>' or result
 end
--- "..."
+
 vim.keymap.set('i', '"', function() return complete('"', true) end, {expr = true})
--- '...'
 vim.keymap.set('i', "'", function() return complete("'", true) end, {expr = true})
--- (...)
-vim.keymap.set('i', '(', '()<left>', {noremap = true})
+vim.keymap.set('i', '(', '()<left>')
 vim.keymap.set('i', ')', function() return complete(')') end, {expr = true})
--- {...}
-vim.keymap.set('i', '{', '{}<left>', {noremap = true})
+vim.keymap.set('i', '{', '{}<left>')
 vim.keymap.set('i', '}', function() return complete('}') end, {expr = true})
--- [...]
-vim.keymap.set('i', '[', '[]<left>', {noremap = true})
+vim.keymap.set('i', '[', '[]<left>')
 vim.keymap.set('i', ']', function() return complete(']') end, {expr = true})
 
 -- visual mode
