642a983a34a103370d5b70fb091116c0d23d5a2c
Ruben Lauwereys <gayp0rn@cash4u.com>
2023-02-04 02:31:48 +0100

    set background color

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -3,9 +3,11 @@
 -----------------
 
 require('notify').setup({
+  background_colour = '#000000',
   minimum_width = 30,
   render = 'default',
 })
 
 vim.notify = require('notify')
+require('telescope').load_extension('notify')
 
