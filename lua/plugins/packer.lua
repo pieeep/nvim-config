7bbccb06d5eccadb5f065a005b4510da9bc8ccfa
pieeep <gayp0rn@cash4u.com>
2023-02-09 02:01:16 +0100

    cleanup

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -4,7 +4,7 @@
 
 -- automatically install packer
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 local packer_bootsrap = false
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({
@@ -15,43 +15,32 @@ if fn.empty(fn.glob(install_path)) > 0 then
     'https://github.com/wbthomason/packer.nvim',
     install_path
   })
-  vim.cmd.packadd 'packer.nvim'
+  vim.cmd.packadd('packer.nvim')
   packer_bootstrap = true
 end
 
 return require('packer').startup({function(use)
   use { 'wbthomason/packer.nvim' }
+  use { 'nvim-lua/plenary.nvim' }
+  use { 'nvim-tree/nvim-web-devicons'}
 
-  -- color schemes
-  use { 'rebelot/kanagawa.nvim' }
+  use { 'rebelot/kanagawa.nvim' } -- color scheme
 
-  -- fancy colorcolumn/indentation
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.1' } -- fuzzy finder
+  use { 'nvim-lualine/lualine.nvim' } -- statusbar
+  use { 'nvim-tree/nvim-tree.lua' } -- file tree
+  use { 'rcarriga/nvim-notify' } -- notification manager
   use { 'lukas-reineke/virt-column.nvim' }
   use { 'lukas-reineke/indent-blankline.nvim' }
 
-  -- notification manager
-  use { 'rcarriga/nvim-notify' }
-
-  -- statusbar
-  use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons' }
-  }
-
-  -- file tree
-  use {
-  'nvim-tree/nvim-tree.lua',
-  requires = { 'nvim-tree/nvim-web-devicons' },
-  }
-
-  -- fuzzy finder
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
   -- parser for better syntax highlighting
-  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
 
   -- sync packer if newly installed
   if packer_bootstrap then
