8b13f507bdacdb4adddb8ffb848ac2c46791227d
pieeep <gayp0rn@cash4u.com>
2023-02-11 18:03:20 +0100

    sync root with current working directory

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require('nvim-tree').setup({
+  sync_root_with_cwd = true,
   view = {
     width = 25,
     preserve_window_proportions = true,
