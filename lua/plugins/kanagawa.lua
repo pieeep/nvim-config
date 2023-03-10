8dc0b85218382510a1bc59613ed7214d1efa4609
pieeep <gayp0rn@cash4u.com>
2023-03-10 05:28:21 +0100

    updatet settings

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,33 +2,39 @@
 -- kanagawa --
 --------------
 
--- Default options:
 require('kanagawa').setup({
+  compile = false,
   undercurl = true,
   commentStyle = { italic = true },
   functionStyle = {},
   keywordStyle = { italic = true},
   statementStyle = { bold = true },
   typeStyle = {},
-  variablebuiltinStyle = { italic = true},
-  specialReturn = true,
-  specialException = true,
   transparent = true,
   dimInactive = false,
-  globalStatus = false,
   terminalColors = true,
-  colors = {},
-  overrides = {
-    StatusLine = { bg = '' },
-    TabLineFill = { bg = '' },
-    ColorColumn = { bg = '' },
-    NormalFloat = { bg = '' },
-    FloatBorder = { bg = '' },
-    TelescopeBorder = { bg = '' },
+  colors = {
+    palette = {},
+    theme = { wave = {}, lotus = {}, dragon = {},
+      all = { ui = { bg_gutter = 'none' } }
+    },
   },
-  theme = 'default'
+  overrides = function(colors)
+    return {
+      StatusLine = { bg = '' },
+      TabLineFill = { bg = '' },
+      ColorColumn = { bg = '' },
+      NormalFloat = { bg = '' },
+      FloatBorder = { bg = '' },
+      TelescopeBorder = { bg = '' },
+    }
+  end,
+  theme = "wave",
+  background = {
+    dark = "wave",
+    light = "lotus"
+  },
 })
 
--- setup must be called before loading
 vim.cmd.colorscheme('kanagawa')
 
